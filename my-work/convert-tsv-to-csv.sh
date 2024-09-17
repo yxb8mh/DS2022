bf49acf255ededf3338eca1c6f5881d8f7b82e16
Gracie Williams <graciewilliams@Gracies-MacBook-Air-2.local>
2024-09-17 10:42:59 -0400

    New changes from class today

---
+++ my-work/convert-tsv-to-csv.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Convert Delimiters
+# To convert TSV to CSV or vice versa, use a text search+replace
+
+# use `tr` - "translate"
+
+
---
+++ my-work/echo-positional-args.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "$0 - is the 0 argument"
+echo "$1 - is the 1 argument"
+echo "$2 - is the 2 argument"
+
+echo "My name is $1 $2"
